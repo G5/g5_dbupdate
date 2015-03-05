c2984b97dcd19b096be3fa237a23d3e0ee89dbc6
Marc Ignacio <padi@users.noreply.github.com>
2015-03-05 15:09:38 +0800

    First semi-working version - Create `g5-dbupdate` executable - add `heroku` and `commander` as dependencies - still need to figure out how to automatically create a binstub   after installing
    [#88210828]

--- g5-dbupdate.gemspec
+++ g5-dbupdate.gemspec
@@ -20,5 +20,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "commander", "~> 4.3.0"
+  spec.add_dependency "commander", "~> 4.3.0"
+  spec.add_dependency "heroku"
 end
--- lib/g5-dbupdate.rb
+++ lib/g5-dbupdate.rb
@@ -1,50 +1,53 @@
-require "g5/dbupdate/version"
+require "g5-dbupdate/version"
 require 'rubygems'
 require 'commander'
 require 'yaml'
 
 module G5
-  class DbUpdate
-    include Commander::Methods
+  module DbUpdate
 
-    def run
-      program :version, '0.0.1'
-      program :description, 'Update local postgresql db from heroku postresql db'
+    class Runner
+      include Commander::Methods
 
-      command :default do |c|
-        c.syntax = 'g5-dbupdate [options]'
-        c.summary = 'Update local postgresql db from heroku postresql db'
-        c.description = 'Update local postgresql db from heroku postresql db'
-        c.example 'description', 'g5-dbupdate --clean --verbose'
-        c.option '--verbose', 'verbose mode'
-        c.option '--local', 'do not fetch from heroku db and use local latest.dump fetched previously'
-        c.option '--clean', 'force removal of latest.dump after '
-        c.action do |args, options|
-          unless options.local
-            app_name = ask("Name of heroku app: ")
-            puts "Fetching latest db dump from heroku app (#{app_name})..."
-            system("curl -o latest.dump `heroku pgbackups:url --app #{app_name}`")
-          end
+      def run
+        program :version, '0.0.1'
+        program :description, 'Update local postgresql db from heroku postresql db'
 
-          db_info = YAML.load_file('config/database.yml')
-          destination_db = db_info["development"]["database"]
-          username = db_info["development"]["username"]
+        command :default do |c|
+          c.syntax = 'g5-dbupdate [options]'
+          c.summary = 'Update local postgresql db from heroku postresql db'
+          c.description = 'Update local postgresql db from heroku postresql db'
+          c.example 'description', 'g5-dbupdate --clean --verbose'
+          c.option '--verbose', 'verbose mode'
+          c.option '--local', 'do not fetch from heroku db and use local latest.dump fetched previously'
+          c.option '--clean', 'force removal of latest.dump after '
+          c.action do |args, options|
+            unless options.local
+              app_name = ask("Name of heroku app: ")
+              puts "Fetching latest db dump from heroku app (#{app_name})..."
+              system("curl -o latest.dump `heroku pgbackups:url --app #{app_name}`")
+            end
 
-          puts "Restoring into #{destination_db}..."
-          verbosity="--verbose" if options.verbose
-          system("pg_restore #{verbosity} --clean --no-acl --no-owner -U #{username} -d #{destination_db} latest.dump")
+            db_info = YAML.load_file('config/database.yml')
+            destination_db = db_info["development"]["database"]
+            username = db_info["development"]["username"]
 
-          if options.clean
-            require 'fileutils'
-            puts "Deleting latest.dump..."
-            FileUtils.rm 'latest.dump'
+            puts "Restoring into #{destination_db}..."
+            verbosity="--verbose" if options.verbose
+            system("pg_restore #{verbosity} --clean --no-acl --no-owner -U #{username} -d #{destination_db} latest.dump")
+
+            if options.clean
+              require 'fileutils'
+              puts "Deleting latest.dump..."
+              FileUtils.rm 'latest.dump'
+            end
           end
         end
+        default_command :default
+        run!
       end
-
-      default_command :default
-
-      run!
     end
+
   end
 end
+
